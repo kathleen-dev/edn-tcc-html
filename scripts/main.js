6dc2d049dee5458ebdf666ea8bf85911f3156a14
Kathleen Caroline Coutinho Benedicto <kathleencaroline.coutinho@gmail.com>
2023-01-19 23:19:39 +0000

    atualização Tcc

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,22 @@ AOS.init({
 });
 
 // Add your javascript here
+function sendEmail(){
+  swal({
+    title: "Quer continuar?",
+    text: "Você vai enviar um e-mail para contratação!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Banco de Currículos", "E-mail enviado com sucesso!", "success");
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+    } else {
+      swal("Your imaginary file is safe!");
+    }
+  });
+}
